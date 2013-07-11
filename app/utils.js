3a2e08af1b9b030ce373b0f6c7a2f387bc1ff927
Robin Tordly <robin.tordly@gmail.com>
2013-07-11 17:16:48 +0200

    removes some css

--- app/utils.js
+++ app/utils.js
@@ -72,7 +72,7 @@ function processGetPlaylists(resultArr) {
     var tmp = '';
 
     for (var i = 0; i < resultArr.length; i++) {
-        insertItemToList("error-menu", 0, resultArr[i].name);
+        insertItemToList("error-menu", i+1, resultArr[i].name);
     };
 
     
