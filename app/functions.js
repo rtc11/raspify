84bec94a44bc3bbbfc215b8a50bcbbebd17f125e
Robin Tordly <robin.tordly@gmail.com>
2013-07-12 18:34:50 +0200

    small change

--- app/functions.js
+++ app/functions.js
@@ -16,44 +16,6 @@ function previous(){
     });
 }
 
-function addTracksToQueue(list){
-	for(var i = 0; i<list.length; i++){
-		addRow(list[i].name, list[i].artists[0].name, list[i].time, list[i].albums.name);
-	}
-}
-
-/********************************************************
- * Add row to queue
- *********************************************************/
-function addRow(track, artist, time, album){
-         if (!document.getElementsByTagName) return;
-         
-         tabBody=document.getElementsByTagName("TBODY").item(0);
-         row=document.createElement("TR");
-         
-         cell1 = document.createElement("TD");
-         cell2 = document.createElement("TD");
-         cell3 = document.createElement("TD");
-         cell4 = document.createElement("TD");
-         
-         textnode1=document.createTextNode(track);
-         textnode2=document.createTextNode(artist);
-         textnode3=document.createTextNode(time);
-         textnode4=document.createTextNode(album);
-
-         cell1.appendChild(textnode1);
-         cell2.appendChild(textnode2);
-         cell3.appendChild(textnode3);
-         cell4.appendChild(textnode4);
-
-         row.appendChild(cell1);
-         row.appendChild(cell2);
-         row.appendChild(cell3);
-         row.appendChild(cell4);
-
-         tabBody.appendChild(row);
-}
-
 /********************************************************
  * Shows the number of playlists
  *********************************************************/
@@ -73,12 +35,4 @@ function showNrOfTracks(nr){
  *********************************************************/
 function insertPlaylist(myid, newListItem) {
     $('ul#' + myid).append('<li><a href="index.html">' + newListItem + '</a></li>');
-}
-
-function addMoreTracksToCounter(nr){
-    window.nrOfTracks += list.length;
-}
-
-function setTrackList(list){
-    window.tracklist = list;
 }
--- app/utils.js
+++ app/utils.js
@@ -34,6 +34,12 @@ function getNrOfTracks(){
     return nrOfTracks;
 }
 
+function addTracksToQueue(list){
+    for(var i = 0; i<list.length; i++){
+        addRow(list[i].name, list[i].artists[0].name, list[i].time, list[i].albums.name);
+    }
+}
+
 function fetchFromMopidy(){
     var consoleError = console.error.bind(console);
 
@@ -79,7 +85,7 @@ function fetchFromMopidy(){
     var putPlaylistsOnGUI = function(list){
         if ((!list) || (list == '')) {return;}
         for (var i = 0; i < list.length; i++) {
-               insertPlaylist("error-menu", i+1, list[i].name);
+               insertPlaylist("error-menu", i+1, list[i].playlist.name);
         };
     }
 
@@ -104,6 +110,38 @@ function fetchFromMopidy(){
 
     mopidy.on("state:online", queueAndPlayFirstPlaylist);
     showNrOfTracks(getNrOfTracks());
+}
+
+/********************************************************
+ * Add row to queue
+ *********************************************************/
+function addRow(track, artist, time, album){
+         if (!document.getElementsByTagName) return;
+         
+         tabBody=document.getElementsByTagName("TBODY").item(0);
+         row=document.createElement("TR");
+         
+         cell1 = document.createElement("TD");
+         cell2 = document.createElement("TD");
+         cell3 = document.createElement("TD");
+         cell4 = document.createElement("TD");
+         
+         textnode1=document.createTextNode(track);
+         textnode2=document.createTextNode(artist);
+         textnode3=document.createTextNode(time);
+         textnode4=document.createTextNode(album);
+
+         cell1.appendChild(textnode1);
+         cell2.appendChild(textnode2);
+         cell3.appendChild(textnode3);
+         cell4.appendChild(textnode4);
+
+         row.appendChild(cell1);
+         row.appendChild(cell2);
+         row.appendChild(cell3);
+         row.appendChild(cell4);
+
+         tabBody.appendChild(row);
 }
 
 /********************************************************
