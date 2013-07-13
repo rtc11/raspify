40ab55bfca3a1220a5689da2b0b0648f9432f558
Robin Tordly <robin.tordly@gmail.com>
2013-07-13 14:31:50 +0200

    added more track info

--- app/utils.js
+++ app/utils.js
@@ -36,7 +36,10 @@ function getNrOfTracks(){
 
 function addTracksToQueue(liste){
     for(var i = 0; i<liste.length; i++){
-        addRow(liste[i].track.name, "-", "-", "-");
+        addRow(liste[i].track.name
+            , liste[i].track.artist.name
+            , liste[i].track.time
+            , liste[i].track.album.name);
     }
 }
 
