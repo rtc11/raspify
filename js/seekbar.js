0c5bb595eb95361786bec1895d498e0a10f0a837
Robin Tordly <robin.tordly@gmail.com>
2014-07-03 15:16:57 +0200

    Fixed seekbar layout and positioning: missing functionality

--- js/seekbar.js
+++ js/seekbar.js
@@ -1,11 +1,30 @@
 //Make sure the document has loaded and that jQuery is ready
-$(document).ready(function() {});
+$(document).ready(function() {
+    self.slider(120);
+    self.setPosition(40);
+});
+
+var totalSecond;
+var self = this;
+
+function slider(total){
+    this.totalSecond = Math.round(total);
+}
+
+function setPosition(time){
+    $(".seekbar").slider('value', time);
+}
 
 $(".seekbar").slider({
     min: 0,
-    max: 100,
-    value: 0,
+    max:100,
+    range: "min",
+    animate: true,
     slide: function(event, ui) {
-        setVolume(ui.value / 100);
+        var range=ui.value;
+        var currentTime=(self.totalSecond*range)/100;
+
+//        console.log("range: " + range + ", total seconds: " + totalSecond);
     }
 });
+
